6efb069ca7873206e61ea06e9a4b0893788968d4
abimsyaefulloh <abimsyaefulloh2020@gmail.com>
2025-09-18 06:52:49 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create base URL API
 export const API = axios.create({
-  baseURL: "http://api.abimft.studentdumbways.my.id/api/v1"
+  baseURL: "https://api.abimft.studentdumbways.my.id/api/v1"
 });
 
 // Set Authorization Token Header
